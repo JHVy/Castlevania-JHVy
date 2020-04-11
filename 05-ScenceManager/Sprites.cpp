3a9232ff36968fb4232399607a3a4f3ff11b22c7
JHVy <16521470@gm.uit.edu.vn>
2020-04-11 16:35:34 +0700

    Fix bug nhay va flip simon theo trc x

---
+++ 05-ScenceManager/Sprites.cpp
@@ -0,0 +1,65 @@
+﻿#include "Sprites.h"
+#include "Game.h"
+#include "Utils.h"
+
+CSprite::CSprite(int id, int left, int top, int right, int bottom, LPDIRECT3DTEXTURE9 tex)
+{
+	this->id = id;
+	this->left = left;
+	this->top = top;
+	this->right = right;
+	this->bottom = bottom;
+	this->texture = tex;
+}
+
+CSprites * CSprites::__instance = NULL;
+
+CSprites *CSprites::GetInstance()
+{
+	if (__instance == NULL) __instance = new CSprites();
+	return __instance;
+}
+
+void CSprite::Draw(float x, float y, int alpha, int isFlip)
+{
+	CGame * game = CGame::GetInstance();
+	if (isFlip == -1)
+	{
+		game->Draw(x, y, texture, left, top, right, bottom, alpha);
+	}
+	else
+	{
+		game->DrawFlipX(x + 32 ,y , texture, left, top, right, bottom, alpha); // khi lật thì nõ sẽ vẽ đối xứng, nên phải trừ đi width của simon để nó vễ đúng position ban đầu. ừaa 
+	}
+}
+
+
+void CSprites::Add(int id, int left, int top, int right, int bottom, LPDIRECT3DTEXTURE9 tex)
+{
+	LPSPRITE s = new CSprite(id, left, top, right, bottom, tex);
+	sprites[id] = s;
+
+	DebugOut(L"[INFO] sprite added: %d, %d, %d, %d, %d \n", id, left, top, right, bottom);
+}
+
+LPSPRITE CSprites::Get(int id)
+{
+	return sprites[id];
+}
+
+/*
+	Clear all loaded textures
+*/
+void CSprites::Clear()
+{
+	for (auto x : sprites)
+	{
+		LPSPRITE s = x.second;
+		delete s;
+	}
+
+	sprites.clear();
+}
+
+
+
