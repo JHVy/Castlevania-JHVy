ba1709f80d3d1142b606ee8a3554c1ba4b385ccf
JHVy <16521470@gm.uit.edu.vn>
2020-06-15 20:18:05 +0700

    Update biến tgian cho board.

--- FullGame/CBoard.cpp
+++ FullGame/CBoard.cpp
@@ -10,7 +10,11 @@ CBoard* CBoard::GetInstance()
 
 void CBoard::Update(DWORD dt)
 {
-	
+	if (GetTickCount() - _count >= 1000)  // 1000 millisecond
+	{
+		_time--;
+		_count = GetTickCount();
+	}
 }
 void CBoard::Render()
 {
@@ -24,14 +28,14 @@ void CBoard::Render()
 	sprites->Get(50000)->Draw(cx + 540, cy);
 
 	// Draw number
-	code->DrawNumber(6, cx + 110, cy + 12, 500); // score
-	code->DrawNumber(4, cx + 295, cy + 12, 300); // time
-	code->DrawNumber(2, cx + 465, cy + 12, 1); // state
-	code->DrawNumber(2, cx + 400, cy + 32, 1); // heart
-	code->DrawNumber(2, cx + 400, cy + 52, 1);
+	code->DrawNumber(6, cx + 110, cy + 12, 500); // score simon->getCore
+	code->DrawNumber(4, cx + 295, cy + 12, _time); // time
+	code->DrawNumber(2, cx + 465, cy + 12, 1); // state  scene->getScene
+	code->DrawNumber(2, cx + 400, cy + 32, 1); // heart  simon->getHeart - xet va cham simon voi tim
+	code->DrawNumber(2, cx + 400, cy + 52, 1); // simon->getLives - check sinh menh cua simon
 
 	// Draw energy bar
-	code->DrawEnergyBar(cx + 110, cy + 30, 0, 5); // player
-	code->DrawEnergyBar(cx + 110, cy + 50, 1, 5); // enemy
+	code->DrawEnergyBar(cx + 110, cy + 30, 0, 5); // player simon->getEnergy - lay gia tri energy cua simon, tru di sau khi va cham voi enemy
+	code->DrawEnergyBar(cx + 110, cy + 50, 1, 5); // enemy  boss->getEnergy 
 
 }
