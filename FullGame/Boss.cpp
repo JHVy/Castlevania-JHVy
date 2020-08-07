c7374f6be94277cf92af490dc64b3febecfca8e7
JHVy <16521470@gm.uit.edu.vn>
2020-08-07 22:41:57 +0700

    fix state

--- FullGame/Boss.cpp
+++ FullGame/Boss.cpp
@@ -11,7 +11,7 @@ Boss::Boss(float _x, float _y, int id) :Enemy(_x, _y, id)
 	AddAnimation(1090);
 	AddAnimation(800);
 
-	_energy = 10;
+	_energy = 16;
 
 	nx = 1;
 	ny = 1;
@@ -46,7 +46,7 @@ void Boss::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 
 	if (dt_die == 0)
 	{
-		if (state == TORCH_STATE_NOT_EXSIST)
+		if (state == CANDLE_STATE_NOT_EXSIST)
 		{
 			dt_die = GetTickCount();
 			if (item)
@@ -155,7 +155,7 @@ void Boss::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 			{
 				item->Update(dt, coObjects);
 				item->GetPosition(x, y);
-				state = TORCH_STATE_ITEM;
+				state = CANDLE_STATE_ITEM;
 			}
 		}
 	}
